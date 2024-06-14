0f564ff7430456018e41f579c3139f088ee0758d
Michael Blaß <m.blass@uke.de>
2024-06-14 13:59:01 +0200

    test: fix pytests

--- tests/test_bindings.py
+++ tests/test_bindings.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 import pytest
 
 import aup3conv as ac
@@ -14,31 +12,22 @@ def project(path):
     return ac.open(path)
 
 
-def test_get_labels(path) -> None:
-    labels = ac.get_labels(path)
-    assert isinstance(labels, list)
-    for item in labels:
+def test_open(project) -> None:
+    assert hasattr(project, "fps")
+    assert hasattr(project, "labels")
+    assert hasattr(project, "load_audio")
+    assert hasattr(project, "path")
+
+def test_fps(project) -> None:
+    assert isinstance(project.fps, int)
+
+def test_labels(project) -> None:
+    assert isinstance(project.labels, list)
+    for item in project.labels:
         assert hasattr(item, "title")
         assert hasattr(item, "start")
         assert hasattr(item, "stop")
 
-def test_open(project) -> None:
-    assert hasattr(project, "path")
-    assert hasattr(project, "labels")
-    assert hasattr(project, "fps")
-
-def test_project_load_waveblock(project) -> None:
-    block = project.load_waveblock(1)
-    assert isinstance(block, list)
-    assert all(isinstance(item, float) for item in block)
-
-
-def test_label_str(path) -> None:
-    labels = ac.get_labels(path)
-    assert labels[0].__str__() == "Label(title='1', start=26.79582766439909, stop=28.978503401360545)"
-
-def test_label_repr(path) -> None:
-    labels = ac.get_labels(path)
-    assert labels[0].__repr__() == "Label(title='1', start=26.79582766439909, stop=28.978503401360545)"
-
-
+def test_load_audio(project) -> None:
+    snd = project.load_audio()
+    assert isinstance(snd, list)
